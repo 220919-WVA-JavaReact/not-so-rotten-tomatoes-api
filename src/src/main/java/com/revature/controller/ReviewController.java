0fd724e41fcdabd3f8e8567190c9857c1e308af6
Tyler Gavrity <tgavrity@gmail.com>
2022-10-28 14:59:19 -0700

    More config

--- src/src/main/java/com/revature/controller/ReviewController.java
+++ src/src/main/java/com/revature/controller/ReviewController.java
@@ -1,4 +1,14 @@
 package com.revature.controller;
 
+import com.revature.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
 public class ReviewController {
+
+    @Autowired
+    private RecipeService rs;
 }
--- src/src/main/java/com/revature/controller/UserController.java
+++ src/src/main/java/com/revature/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.revature.controller;
 
+import com.revature.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
-    //I am here to
+
+    @Autowired
+    private UserService us;
 }
