60a3d49fa14e7b4eebe0d87c28c28e0f1bca1025
Patrick Grady <patrickrgrady81@gmail.com>
2022-11-02 17:55:27 -0400

    fixed infinite loops

--- src/main/java/com/revature/repositories/RecipeRepository.java
+++ src/main/java/com/revature/repositories/RecipeRepository.java
@@ -1,7 +1,9 @@
 package com.revature.repositories;
 
+import com.revature.entities.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecipeRepository {
+public interface RecipeRepository extends JpaRepository<Recipe, String> {
 }
