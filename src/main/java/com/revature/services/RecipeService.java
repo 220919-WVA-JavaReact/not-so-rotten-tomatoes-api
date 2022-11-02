c41518379937185e7d8e1e255750dade0864d0c6
Claire List <claire.list2501@gmail.com>
2022-11-02 14:58:25 -0600

    repo fun

--- src/main/java/com/revature/services/RecipeService.java
+++ src/main/java/com/revature/services/RecipeService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
+    //I NOW directly call recipe repository r.updateById(id, update) or some such. I  may need to be manually done, we will see.
+    //TODO: RESEARCH HOW PATCH REQUESTS WORK IN JPA repository!
+    //@Query("UPDATE recipes SET instructions = ?2 WHERE recipe_id = ?2 RETURNING *")
+    //Recipe update(int id, String update)
+    //if this does not work, try with nativeQuery = true in the above query string.
 }
