0b75e1d75c20d802414132c27d0389f2ae909ca6
Claire List <claire.list2501@gmail.com>
2022-11-03 13:11:54 -0600

    some TDD fun

---
+++ src/test/java/com/revature/recipepatch/PatchReqIsWired.java
@@ -0,0 +1,19 @@
+package com.revature.recipepatch;
+
+//imports:: NEED recipe DAO, model, service (so we can mock these calls!)
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class PatchReqIsWired {
+
+}
