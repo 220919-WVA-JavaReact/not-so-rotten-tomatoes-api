a0f6b77e503014dc3b74d18f956cb60592ac80d6
12cwarden <12cwarden@gmail.com>
2022-11-03 15:07:20 -0400

    added remaining logging

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -1,8 +1,11 @@
 package com.revature.controllers;
 
+import antlr.Token;
 import com.revature.dtos.LoginDTO;
 import com.revature.dtos.UserDTO;
 import com.revature.services.AuthService;
+import com.revature.services.TokenService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,19 +13,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private AuthService as;
+    private TokenService ts;
 
-    public AuthController(AuthService as) {
+    public AuthController(AuthService as, TokenService ts) {
         this.as = as;
+        this.ts = ts;
     }
     @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<UserDTO> authenticate(@RequestBody LoginDTO login) {
         UserDTO user = as.authenticate(login);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        String token = ts.generateToken(user);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorzation", token);
+        return new ResponseEntity<>(user, headers, HttpStatus.OK);
     }
 
 
