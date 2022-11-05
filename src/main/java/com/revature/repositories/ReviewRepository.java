784cee889f997098c794afa5e44ce083cb1e8e19
Claire List <claire.list2501@gmail.com>
2022-11-05 08:58:07 -0600

    wats for breakky

--- src/main/java/com/revature/repositories/ReviewRepository.java
+++ src/main/java/com/revature/repositories/ReviewRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    //TODO WHEN BACK FROM BREAKKY:: I NEED TO DEFINE MY METHODS HERE!
 }
