d173ad55277bb11bc592e29eb0d1df1c7a7ea6e3
Claire List <claire.list2501@gmail.com>
2022-11-05 10:21:08 -0600

    if its worth doing its worth doing poorly

--- src/main/java/com/revature/repositories/RecipeRepository.java
+++ src/main/java/com/revature/repositories/RecipeRepository.java
@@ -2,9 +2,14 @@ package com.revature.repositories;
 
 import com.revature.entities.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+    @Query(value = "SELECT * FROM recipes WHERE recipe_id = ?1")
+    Recipe findById(int id);
 
+    @Query("UPDATE recipes SET instructions = ?2 WHERE recipe_id = ?2 RETURNING *")
+    Recipe updateInstructions(int id, String update);
 }
--- src/main/java/com/revature/services/RecipeService.java
+++ src/main/java/com/revature/services/RecipeService.java
@@ -27,30 +27,25 @@ public class RecipeService {
     //Recipe update(int id, String update)
     //if this does not work, try with nativeQuery = true in the above query string.
     // syntax for nativeQuery:: @Query(value = "SELECT * FROM USERS WHERE EMAIL_ADDRESS = ?1", nativeQuery = true)
-    @Query("UPDATE recipes SET instructions = ?2 WHERE recipe_id = ?2 RETURNING *")
+
     public Recipe updateRecipe(int id, String update){
-       // Recipe r = new Recipe();
-        if (rr.findById(id).isPresent()){
-//            User user = ur.findById(id).orElseThrow(() -> new UserNotFoundException());
-//            UserDTO userDTO = new UserDTO(user);
+//       // Recipe r = new Recipe();
+//        if (rr.findById(id).isPresent()){
+////            User user = ur.findById(id).orElseThrow(() -> new UserNotFoundException());
+////            UserDTO userDTO = new UserDTO(user);
+////
+////            return userDTO;
+//            Optional<Recipe> updated = rr.findById(id);
 //
-//            return userDTO;
-            Optional<Recipe> updated = rr.findById(id);
+//            Recipe up = new Recipe(updated);
+//          //  up.setInstructions(update);
+//            return up; //currently, returning NOTHING !
 
-            Recipe up = new Recipe(updated);
-          //  up.setInstructions(update);
-            return up; //currently, returning NOTHING !
-
-        } else {
-            return null;
-        }
-
+//        } else {
+//            return null;
+//        }
+    return null;
 
     }
 
-    public Optional<Recipe> findById(int id){
-        //isPresent() will return true if an object is returned, false if no object.
-        //we can use this for verify on this method!
-        return rr.findById(id);
-    }
 }
