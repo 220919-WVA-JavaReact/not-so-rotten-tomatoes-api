721d5b32c44e03e59370f9455437939d43295392
Claire List <claire.list2501@gmail.com>
2022-11-05 11:09:20 -0600

    exception added to call maybe it will work now

--- src/main/java/com/revature/services/RecipeService.java
+++ src/main/java/com/revature/services/RecipeService.java
@@ -1,6 +1,7 @@
 package com.revature.services;
 
 import com.revature.entities.Recipe;
+import com.revature.exceptions.RecipeNotFoundException;
 import com.revature.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
@@ -23,28 +24,18 @@ public class RecipeService {
         return rr.findAll();
     }
 
-    //@Query("UPDATE recipes SET instructions = ?2 WHERE recipe_id = ?2 RETURNING *")
-    //Recipe update(int id, String update)
-    //if this does not work, try with nativeQuery = true in the above query string.
-    // syntax for nativeQuery:: @Query(value = "SELECT * FROM USERS WHERE EMAIL_ADDRESS = ?1", nativeQuery = true)
 
-    public Recipe updateRecipe(int id, String update){
-//       // Recipe r = new Recipe();
-//        if (rr.findById(id).isPresent()){
-////            User user = ur.findById(id).orElseThrow(() -> new UserNotFoundException());
-////            UserDTO userDTO = new UserDTO(user);
-////
-////            return userDTO;
-//            Optional<Recipe> updated = rr.findById(id);
-//
-//            Recipe up = new Recipe(updated);
-//          //  up.setInstructions(update);
-//            return up; //currently, returning NOTHING !
+    public Recipe updateRecipe(int id, String update) throws RecipeNotFoundException{
+        Recipe recipe = null;
+        try {
+             recipe = rr.updateInstructions(id, update);
+        } catch (RecipeNotFoundException r){
+            r.getMessage();
+        }
 
-//        } else {
-//            return null;
-//        }
-    return null;
+        // I need to return Optional<Recipe> if I want to use
+        //the .orElseThrow method.Howver, I only want to return either a recipe or null , here.
+    return recipe;
 
     }
 
