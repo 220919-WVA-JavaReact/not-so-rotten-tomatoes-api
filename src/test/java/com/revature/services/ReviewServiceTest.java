58f6ca93da0b7e0160c1945ef1e27bc8650ff14b
Cory W <12cwarden@gmail.com>
2022-11-07 12:59:56 -0500

    junit testing review

--- src/test/java/com/revature/services/ReviewServiceTest.java
+++ src/test/java/com/revature/services/ReviewServiceTest.java
@@ -7,12 +7,16 @@ import com.revature.entities.User;
 import com.revature.exceptions.ReviewNotFoundException;
 import com.revature.repositories.ReviewRepository;
 
+import org.checkerframework.checker.nullness.Opt;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -75,7 +79,74 @@ public class ReviewServiceTest {
         expected.setReview_text("good food");
         expected.setRecipe_id(testRecipe);
 
-        Review actual = sut.getReviewById(1);
+        Review actual = sut.deleteReviewById(1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void deleteReviewByIdDoesNotExist(){
+        Mockito.when(mockRepository.findById(100)).thenReturn(Optional.empty());
+
+        assertThrows(ReviewNotFoundException.class, () -> sut.deleteReviewById(100));
+    }
+
+    @Test
+    public void getAllReviewsRetrieves(){
+        User cory = new User();
+        Recipe testRecipe = new Recipe();
+        Review returnedReview = new Review();
+        returnedReview.setReview_id(1);
+        returnedReview.setAuthor(cory);
+        returnedReview.setReview_text("good food");
+        returnedReview.setRecipe_id(testRecipe);
+        List<Review> returned = new ArrayList<>();
+        returned.add(returnedReview);
+
+        Mockito.when(mockRepository.findAll()).thenReturn(returned);
+
+        Review expected = new Review();
+        expected.setReview_id(1);
+        expected.setAuthor(cory);
+        expected.setReview_text("good food");
+        expected.setRecipe_id(testRecipe);
+        List<Review> expect = new ArrayList<>();
+        expect.add(expected);
+
+        List<Review> actual = sut.getAllReviews();
+
+
+        assertEquals(expect, actual);
+    }
+
+//    @Test
+//    public void getAllReviewsDoesNotExist(){
+//        Mockito.when(mockRepository.findAll()).thenReturn(Optional.empty());
+//
+//        assertThrows(ReviewNotFoundException.class, () -> sut.getAllReviews());
+//    }
+
+    @Test
+    public void createReviewSaves(){
+        User cory = new User();
+        Recipe testRecipe = new Recipe();
+        Review returnedReview = new Review();
+        returnedReview.setReview_id(1);
+        returnedReview.setAuthor(cory);
+        returnedReview.setReview_text("good food");
+        returnedReview.setRecipe_id(testRecipe);
+
+
+        Mockito.when(mockRepository.save(returnedReview)).thenReturn(returnedReview);
+
+        Review expected = new Review();
+        expected.setReview_id(1);
+        expected.setAuthor(cory);
+        expected.setReview_text("good food");
+        expected.setRecipe_id(testRecipe);
+
+
+        Review actual = sut.createReview(returnedReview);
 
         assertEquals(expected, actual);
     }
